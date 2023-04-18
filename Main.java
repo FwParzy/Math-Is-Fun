7278590e9b5c060be18676d9ef9c095f3e50d493
LivelyAndrew <70732859+LivelyAndrew@users.noreply.github.com>
2023-04-18 16:57:08 -0400

    added data validation for menu choices

--- Main.java
+++ Main.java
@@ -1,82 +1,111 @@
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int choice;
 
-    do {
-      System.out.println("1. Login");
-      System.out.println("2. Add user");
-      System.out.println("3. Search user by ID");
-      System.out.println("4. Print all users");
-      System.out.println("5. Exit");
-      System.out.print("Enter your choice: ");
-      choice = scanner.nextInt();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        User currentUser; // use to track performance of user. 
 
-      switch (choice) {
-        case 1:
-          login(scanner);
-          break;
-        case 2:
-          addUser(scanner);
-          break;
-        case 3:
-          searchUserById(scanner);
-          break;
-        case 4:
-          printAllUsers();
-          break;
-        case 5:
-          System.out.println("Goodbye!");
-          break;
-        default:
-          System.out.println("Invalid choice. Please try again.");
-      }
-    } while (choice != 5);
+        do {
 
-    scanner.close();
-  }
+            do {
+                System.out.println("1. Login");
+                System.out.println("2. Add user");
+                System.out.println("3. Search user by ID");
+                System.out.println("4. Print all users");
+                System.out.println("5. Exit");
+                System.out.print("Enter your choice: ");
+                while (!scanner.hasNextInt()) {
+                    System.out.println("must be a number: ");
+                    scanner.next();
+                }
+                choice = scanner.nextInt();
+            } while (choice < 1 || choice > 5);
 
-  private static void login(Scanner scanner) {
-    System.out.print("Enter username: ");
-    String username = scanner.next();
+            switch (choice) {
+                case 1:
+                    login(scanner);
+                    break;
+                case 2:
+                    addUser(scanner);
+                    break;
+                case 3:
+                    searchUserById(scanner);
+                    break;
+                case 4:
+                    printAllUsers();
+                    break;
+                case 5:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice
+                != 5);
 
-    System.out.print("Enter password: ");
-    String password = scanner.next();
-
-    if (!Login.authenticateUser(username, password)) {
-      System.out.println("Invalid username or password.");
+        scanner.close();
     }
-    System.out.println("Login successful. Hello ");
-  }
 
-  private static void addUser(Scanner scanner) {
-    System.out.print("Enter email: ");
-    String email = scanner.next();
+    private static void login(Scanner scanner) {
+        System.out.print("Enter username: ");
+        String username = scanner.next();
 
-    System.out.print("Enter first name: ");
-    String firstName = scanner.next();
+        System.out.print("Enter password: ");
+        String password = scanner.next();
 
-    System.out.print("Enter last name: ");
-    String lastName = scanner.next();
+        if (!Login.authenticateUser(username, password)) {
+            System.out.println("Invalid username or password.");
+        }
+        System.out.println("Login successful. Hello ");
+    }
 
-    System.out.print("Enter username: ");
-    String username = scanner.next();
+    private static void addUser(Scanner scanner) {
+        System.out.print("Enter email: ");
+        String email = scanner.next();
 
-    System.out.print("Enter password: ");
-    String password = scanner.next();
+        int level = 0;
 
-    Login.addUser(email, firstName, lastName, username, password);
-  }
+        do {
+            System.out.print("Enter level(1-4): ");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Please enter a number (1-4): ");
+                scanner.next();
+            }
+            level = scanner.nextInt();
+        } while (level > 4 || level < 1);
 
-  private static void searchUserById(Scanner scanner) {
-    System.out.print("Enter user ID: ");
-    int id = scanner.nextInt();
-    User.searchUserById(id);
-  }
+        System.out.print("Enter first name: ");
+        String firstName = scanner.next();
 
-  private static void printAllUsers() {
-    User.printAllUsers();
-  }
+        System.out.print("Enter last name: ");
+        String lastName = scanner.next();
+
+        System.out.print("Enter username: ");
+        String username = scanner.next();
+
+        System.out.print("Enter password: ");
+        String password = scanner.next();
+
+        Login.addUser(level, email, firstName, lastName, username, password);
+    }
+
+    private static void searchUserById(Scanner scanner) {
+        int id =0;
+        do {
+            System.out.print("Enter user ID: ");
+            while (!scanner.hasNextInt()) {
+                System.out.print("must be a number ");
+                scanner.next();
+            }
+            id = scanner.nextInt();
+        } while (id < 1 || id > Database.getNextId() - 1);
+
+        Database.searchUserById(id);
+    }
+
+    private static void printAllUsers() {
+        Database.printAllUsers();
+    }
 }
